988df5f324bd9d622f285e40a96e6c05c712f00e
jnarvaez <joseluis.narvaez0@gmail.com>
2018-03-09 00:11:17 -0300

    Paso a spring mvc

---
+++ src/main/java/cl/startToken/controllers/ClienteController.java
@@ -0,0 +1,15 @@
+package cl.startToken.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ClienteController {
+	
+	
+	public ModelAndView crearCliente() {
+		return null;
+		
+	}
+
+}
