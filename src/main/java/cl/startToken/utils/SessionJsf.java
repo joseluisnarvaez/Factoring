c43bc093de4c5782e163f130607344cf51c43c26
Monito <Monito@LAPTOP-0SEKV9KU>
2018-06-20 11:04:11 -0400

    session jsf

---
+++ src/main/java/cl/startToken/utils/SessionJsf.java
@@ -0,0 +1,39 @@
+package cl.startToken.utils;
+
+import java.io.IOException;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import cl.startToken.to.Usuario;
+/**
+ * clase que valida si el usuario tiene session inicializada
+ * @author JNarvaez
+ *
+ */
+public class SessionJsf {
+	
+	
+	public static void seteaSession(Usuario usuario) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+		usuario.setContrasenia("");
+		session.setAttribute("usuario", usuario);
+	}
+	
+	
+	public static void validaSession() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+		if(usuario == null) {
+			FacesContext contex = FacesContext.getCurrentInstance();
+			try {
+				contex.getExternalContext().redirect( "/Factoring" );
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
